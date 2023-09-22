ba218a422beba860f55e09b70b1ce4a60c531925
Ka4insky <rushan.garaev28@gmail.com>
2023-09-22 19:36:43 +0300

    Добавьте файлы проекта.

---
+++ MIREA_FIRST_LAB/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIREA_FIRST_LAB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("END OF THE WORLD");
+            Console.ReadLine();
+        }
+    }
+}
