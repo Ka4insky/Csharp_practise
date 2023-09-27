21f83d69071b0ef181640a910f17632704ae80cf
Ka4insky <rushan.garaev28@gmail.com>
2023-09-27 18:01:01 +0300

    first task

--- MIREA_FIRST_LAB/Program.cs
+++ MIREA_FIRST_LAB/Program.cs
@@ -10,8 +10,51 @@ namespace MIREA_FIRST_LAB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("END OF THE WORLD");
-            Console.ReadLine();
+            FirstPositiveLastNegative();
+
+            void FirstPositiveLastNegative()
+            {
+                bool positive = false;
+                int negative = 0;
+
+
+                Console.WriteLine("Введите последовательность целых чисел через пробел:");
+                int[] arr = Console.ReadLine().Split(' ').
+                    Select(x => int.Parse(x)).ToArray();
+
+                foreach (int x in arr)
+                {
+                    if (x > 0 & !positive)
+                    {
+                        Console.WriteLine(x);
+                        positive = true;
+                        continue;
+                    }
+
+                    if (x < 0) 
+                    {
+                        negative = x;
+                    }
+                }
+
+                if (!positive) 
+                {
+                    Console.WriteLine("Положительных чисел нет :(");
+                }
+
+                if (negative == 0)
+                {
+                    Console.WriteLine("Отрицательных чисел нет :(");
+                }
+                else 
+                {
+                    Console.WriteLine(negative);
+                }
+
+                Console.ReadLine();
+            }
         }
+
+        
     }
 }
